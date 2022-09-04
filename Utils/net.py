583cd7792157585d6ac442d17b7114b8c46e7777
jianlgler <74598295+jianlgler@users.noreply.github.com>
2022-09-04 12:05:49 +0200

    Add files via upload

--- Utils/net.py
+++ Utils/net.py
@@ -5,215 +5,173 @@ import torch.optim as optim
 import os
 from .utils import *
 
-# The style transfer network
-class Net(nn.Module):
-  def __init__(self,
-               device, # "cpu" for cpu, "cuda" for gpu
-               enc_state_dict, # The state dict of the pretrained vgg19
-               learning_rate=1e-4,
-               learning_rate_decay=5e-5, # Decay parameter for the learning rate
-               gamma=2.0, # Controls importance of StyleLoss vs ContentLoss, Loss = gamma*StyleLoss + ContentLoss
-               train=True, # Wether or not network is training
-               load_fromstate=False, # Load from checkpoint?
-               load_path=None # Path to load checkpoint
-               ):
-    super().__init__()
+decoder_re = nn.Sequential(
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 256, (3, 3)),
+    nn.ReLU(), #relu 4_1
+    nn.Upsample(scale_factor=2, mode='nearest'),
 
-    assert device in ["cpu", "cuda"]
-    if load_fromstate and not os.path.isfile(load_path):
-      raise ValueError("Checkpoint file not found")
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(), #relu 3_4 
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(), #relu 3_3
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(), #relu 3_2
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 128, (3, 3)),
+    nn.ReLU(), #relu 3_1
+    nn.Upsample(scale_factor=2, mode='nearest'),
 
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(128, 128, (3, 3)),
+    nn.ReLU(), 
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(128, 64, (3, 3)),
+    nn.ReLU(), 
+    nn.Upsample(scale_factor=2, mode='nearest'),
 
-    self.learning_rate = learning_rate
-    self.learning_rate_decay = learning_rate_decay
-    self.train = train
-    self.gamma = gamma
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(64, 64, (3, 3)),
+    nn.ReLU(), 
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(64, 3, (3, 3)),
+)
 
-    self.encoder = Encoder(enc_state_dict, device) # A pretrained vgg19 is used as the encoder
-    self.decoder = Decoder().to(device)
+vgg_re = nn.Sequential(
+    nn.Conv2d(3, 3, (1, 1)),
 
-    self.optimiser = optim.Adam(self.decoder.parameters(), lr=self.learning_rate)
-    self.iters = 0
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(3, 64, (3, 3)),
+    nn.ReLU(),  # relu1-1
 
-    if load_fromstate:
-      state = torch.load(load_path)
-      self.decoder.load_state_dict(state["Decoder"])
-      self.optimiser.load_state_dict(state["Optimiser"])
-      self.iters = state["iters"]
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(64, 64, (3, 3)),
+    nn.ReLU(),  # relu1-2
 
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
 
-  def set_train(self, boolean): # Change state of network
-    assert type(boolean) == bool
-    self.train = boolean
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(64, 128, (3, 3)),
+    nn.ReLU(),  # relu2-1
 
-  def adjust_learning_rate(self, optimiser, iters): # Simple learning rate decay
-    lr = self.learning_rate / (1.0 + self.learning_rate_decay * iters)
-    for param_group in optimiser.param_groups:
-        param_group['lr'] = lr
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(128, 128, (3, 3)),
+    nn.ReLU(),  # relu2-2
 
-  def forward(self, style, content, alpha=1.0): # Alpha can be used while testing to control the importance of the transferred style
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
 
-    # Encode style and content
-    syle_feats = self.encoder(style, self.train) # if train: returns all states
-    content_feat = self.encoder(content, False) # for the content only the last layer is important
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(128, 256, (3, 3)),
+    nn.ReLU(),  # relu3-1
 
-    # Transfer Style
-    if self.train:
-      t = ada_in(content_feat, syle_feats[-1]) # Last layer is "style" layer
-    else:
-      t = alpha*ada_in(content_feat, syle_feats) + (1-alpha)*content_feat # Alpha controls magnitude of style
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(),  # relu3-2
 
-    # Scale up
-    g_t = self.decoder(t)
-    if not self.train:
-      return g_t # When not training return transformed image
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(),  # relu3-3
 
-    # Compute Loss
-    g_t_features = self.encoder(g_t, self.train)
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(),  # relu3-4
 
-    content_loss = Content_loss(g_t_features[-1], content_feat)
-    style_loss = Style_loss(g_t_features, syle_feats)
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 512, (3, 3)),
+    nn.ReLU(),  # relu4-1, this is the last layer used
 
-    loss_comb = content_loss + self.gamma*style_loss
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu4-2
 
-    return loss_comb, content_loss, style_loss
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu4-3
 
-# The decoder is a reversed vgg19 up to ReLU 4.1. To note is that the last layer is not activated.
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu4-4
 
-class Decoder(nn.Module):
-  def __init__(self):
-    super().__init__()
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
 
-    self.padding = nn.ReflectionPad2d(padding=1) # Using reflection padding as described in vgg19
-    self.UpSample = nn.Upsample(scale_factor=2, mode="nearest")
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu5-1
 
-    self.conv4_1 = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=3, stride=1, padding=0)
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu5-2
 
-    self.conv3_4 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=0)
-    self.conv3_3 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=0)
-    self.conv3_2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=0)
-    self.conv3_1 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=1, padding=0)
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu5-3
 
-    self.conv2_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=0)
-    self.conv2_1 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=0)
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU()  # relu5-4
+)
 
-    self.conv1_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=0)
-    self.conv1_1 = nn.Conv2d(in_channels=64, out_channels=3, kernel_size=3, stride=1, padding=0)
+class Net_re(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(Net_re, self).__init__()
+        enc_layers = list(encoder.children())
+        self.enc_1 = nn.Sequential(*enc_layers[:4])  # input -> relu1_1
+        self.enc_2 = nn.Sequential(*enc_layers[4:11])  # relu1_1 -> relu2_1
+        self.enc_3 = nn.Sequential(*enc_layers[11:18])  # relu2_1 -> relu3_1
+        self.enc_4 = nn.Sequential(*enc_layers[18:31])  # relu3_1 -> relu4_1
+        self.decoder = decoder
+        self.mse_loss = nn.MSELoss()
+        self.iters = 0
 
+        # fix the encoder
+        for name in ['enc_1', 'enc_2', 'enc_3', 'enc_4']:
+            for param in getattr(self, name).parameters():
+                param.requires_grad = False
 
-  def forward(self, x):
-    out = self.UpSample(F.relu(self.conv4_1(self.padding(x))))
+    # extract relu1_1, relu2_1, relu3_1, relu4_1 from input image
+    def encode_with_intermediate(self, input):
+        results = [input]
+        for i in range(4):
+            func = getattr(self, 'enc_{:d}'.format(i + 1))
+            results.append(func(results[-1]))
+        return results[1:]
 
-    out = F.relu(self.conv3_4(self.padding(out)))
-    out = F.relu(self.conv3_3(self.padding(out)))
-    out = F.relu(self.conv3_2(self.padding(out)))
-    out = self.UpSample(F.relu(self.conv3_1(self.padding(out))))
+    # extract relu4_1 from input image
+    def encode(self, input):
+        for i in range(4):
+            input = getattr(self, 'enc_{:d}'.format(i + 1))(input)
+        return input
 
-    out = F.relu(self.conv2_2(self.padding(out)))
-    out = self.UpSample(F.relu(self.conv2_1(self.padding(out))))
+    def calc_content_loss(self, input, target):
+        assert (input.size() == target.size())
+        assert (target.requires_grad is False)
+        return self.mse_loss(input, target)
 
-    out = F.relu(self.conv1_2(self.padding(out)))
-    out = self.conv1_1(self.padding(out))
-    return out
+    def calc_style_loss(self, input, target):
+        assert (input.size() == target.size())
+        assert (target.requires_grad is False)
+        input_mean, input_std = calc_mean_std(input)
+        target_mean, target_std = calc_mean_std(target)
+        return self.mse_loss(input_mean, target_mean) + \
+               self.mse_loss(input_std, target_std)
 
-# A vgg19 Sequential which is used up to Relu 4.1. To note is that the
-# first layer is a 3,3 convolution, different from a standard vgg19
+    def forward(self, content, style, alpha=1.0):
+        assert 0 <= alpha <= 1
+        style_feats = self.encode_with_intermediate(style)
+        content_feat = self.encode(content)
+        t = ada_in(content_feat, style_feats[-1])
+        t = alpha * t + (1 - alpha) * content_feat
 
-class Encoder(nn.Module):
-    def __init__(self, state_dict, device):
-        super().__init__()
-        self.vgg19 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=3, kernel_size=1),
+        g_t = self.decoder(t)
+        g_t_feats = self.encode_with_intermediate(g_t)
 
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 1_1
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 1_2
-
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=True),
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 2_1
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 2_2
-
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=True),
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 3_1
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 3_2
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 3_3
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 3_4
-
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=True),
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 4_1 ===> Output layer of the decoder
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 4_2
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 4_3
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 4_4
-
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=True),
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 5_1
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 5_2
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3),
-            nn.ReLU(inplace=True), # relu 5_3
-
-            nn.ReflectionPad2d(padding=1),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3),
-            nn.ReLU(inplace=True) # relu 5_4
-            ).to(device)
-
-        self.vgg19.load_state_dict(state_dict)
-
-        encoder_children = list(self.vgg19.children())
-        self.EncoderList = nn.ModuleList([nn.Sequential(*encoder_children[:4]), # Up to Relu 1.1
-                                          nn.Sequential(*encoder_children[4:11]), # Up to Relu 2.1
-                                          nn.Sequential(*encoder_children[11:18]), # Up to Relu 3.1
-                                          nn.Sequential(*encoder_children[18:31]), # Up to Relu 4.1, also the
-                                          ])                                       # input for the decoder
-
-    def forward(self, x, intermediates=False): # if training use intermediates = True, to get the output of
-        states = []                            # all the encoder layers to calculate the style loss
-        for i in range(len(self.EncoderList)):
-            x = self.EncoderList[i](x)
-
-            if intermediates:       # All intermediate states get saved in states
-                states.append(x)
-        if intermediates:
-            return states
-        return x
+        loss_c = self.calc_content_loss(g_t_feats[-1], t)
+        loss_s = self.calc_style_loss(g_t_feats[0], style_feats[0])
+        for i in range(1, 4):
+            loss_s += self.calc_style_loss(g_t_feats[i], style_feats[i])
+        return loss_c, loss_s
